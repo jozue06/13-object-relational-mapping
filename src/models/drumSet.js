98899b90adb5ab378fdae20294b263b4efbc61d8
Jozue06 <jozue06@gmail.com>
2018-06-22 21:03:32 -0700

    on the track again

--- src/models/drumSet.js
+++ src/models/drumSet.js
@@ -1,29 +1,9 @@
 'use strict';
 
-import mongoose from 'mongoose';
+import mongoose, {Schema} from 'mongoose';
 
-const newSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  position: {
-    type: String,
-    uppercase: true,
-    required: true,
-  },
-  bats: {
-    type: String,
-    required: true,
-    default: 'R',
-    enum: ['R', 'r', 'L', 'l'],
-  },
-  throws: {
-    type: String,
-    required: true,
-    default: 'R',
-    enum: ['R', 'r', 'L', 'l'],
-  },
+const schema = new Schema({
+  name : String,
 });
 
-export default mongoose.model('model', newSchema);
+export default mongoose.model('Band', schema);
